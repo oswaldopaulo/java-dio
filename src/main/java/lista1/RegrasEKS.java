5293c08d01cecc77d14691844936384e9ba81407
oswaldopaulo <oswaldo.paulo@gmail.com>
2023-08-10 09:24:16 -0300

    lista 4

--- src/main/java/lista1/RegrasEKS.java
+++ src/main/java/lista1/RegrasEKS.java
@@ -4,35 +4,29 @@ import java.lang.Math;
 public class RegrasEKS {
     public static void main(String[] args) {
         // Definindo as regras básicas
-        int podsPorNode = 10;
-        int podsPorServidor = 4;
+        double podsPorNode = 10;
+        double podsPorServidor = 4;
         String msg1, msg2;
 
         // Criando um objeto Scanner para receber as entradas do usuário
         Scanner scanner = new Scanner(System.in);
 
         // Recebendo as informações do usuário
-        int numeroTotalPods = scanner.nextInt();
-
+        double numeroTotalPods = scanner.nextInt();
 
         //TODO: Calcule o número mínimo de nodes necessários
-        int numeroMinimoNodes = (int) Math.ceil( numeroTotalPods / podsPorNode);
-
-
-
-
-
-
+        double numeroMinimoNodes =   numeroTotalPods / podsPorNode;
         //TODO: Calcule o número mínimo de servidores necessários
-        int numeroMinimoServidores = (int) Math.ceil(numeroTotalPods / podsPorServidor);
+        double numeroMinimoServidores =  numeroTotalPods / podsPorServidor;
 
         //TODO: Exiba o resultado com as recomendações de quantidades de servidores e nodes.
 
-        msg1 = (numeroMinimoNodes < 1 ) ? "Recomendamos usar um unico node" : "Numero minimo de nodes:" + numeroMinimoNodes;
-        msg2 = (numeroMinimoServidores < 1 ) ? "Recomendamos usar um unico servidor" : "Numero minimo de servidores:" + numeroMinimoServidores;
 
-        System.out.println(msg1);
-        System.out.println(msg2);
+        msg1 = (numeroMinimoNodes <= 1 ) ? "Recomendamos usar um unico node" : "Numero minimo de nodes:" + (int) Math.ceil(numeroMinimoNodes);
+        msg2 = (numeroMinimoServidores <= 1 ) ? "Recomendamos usar um unico servidor" : "Numero minimo de servidores:" + (int) Math.ceil(numeroMinimoServidores);
+
+        System.out.println("1." + msg1);
+        System.out.println("2."+ msg2);
         // Fechando o Scanner
         scanner.close();
     }
